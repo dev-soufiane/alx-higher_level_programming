f91f6e263771e2f742c04b0fec18a677025b7762
dev-soufiane <dev.sofyane@gmail.com>
2023-10-31 16:39:55 -0700

    Update 100-print_tebahpla.

--- 0x01-python-if_else_loops_functions/100-print_tebahpla.py
+++ 0x01-python-if_else_loops_functions/100-print_tebahpla.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 
-for n in range(ord('Z'), ord('A') - 1, -1):
-    if n % 2 == 0:
-        print(chr(n + 32), end='')
+for i in range(ord('Z'), ord('A') - 1, -1):
+    if i % 2 == 0:
+        print("{}".format(chr(i + 32)), end='')
     else:
-        print(chr(n), end='')
+        print("{}".format(chr(i)), end='')
