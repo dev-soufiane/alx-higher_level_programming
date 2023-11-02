b46d0c238f5100d141cbead34cf2ac52253fe88f
dev-soufiane <dev.sofyane@gmail.com>
2023-11-02 14:15:07 -0700

    Add double '=' in if condition

--- 0x02-python-import_modules/0-add.py
+++ 0x02-python-import_modules/0-add.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 
-if __name__ = "__main__":
+if __name__ == "__main__":
     """This program calculates the sum of a and b, and prints the result"""
     from add_0 import add
     a = 1
