ffa0e183fe01bb9fdebb8207903e9d59566efca7
dev-soufiane <dev.sofyane@gmail.com>
2023-11-02 14:08:27 -0700

    Add 0-add script

---
+++ 0x02-python-import_modules/0-add.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+
+if __name__ = "__main__":
+    """This program calculates the sum of a and b, and prints the result"""
+    from add_0 import add
+    a = 1
+    b = 2
+    print("{} + {} = {}".format(a, b, add(a, b)))
