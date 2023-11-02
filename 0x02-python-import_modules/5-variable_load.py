c89bc9fabbc44f26289b72500ffdd4dd9c68977e
dev-soufiane <dev.sofyane@gmail.com>
2023-11-02 15:56:29 -0700

    Add 5-variable_load script

---
+++ 0x02-python-import_modules/5-variable_load.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+if __name__ == "__main__":
+    """This program that imports the variable a from the file
+    variable_load_5.py and prints its value."""
+
+    from variable_load_5 import a
+    print("{}".format(a))
