bcff1223109a1087bf33e8b5860457101bc034a7
dev-soufiane <dev.sofyane@gmail.com>
2023-11-06 14:56:50 -0800

    Add 10-divisible_by_2 script

---
+++ 0x03-python-data_structures/10-divisible_by_2.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+
+def divisible_by_2(my_list=[]):
+    """Function that finds all multiples of 2 in a list."""
+
+    new_list = []
+
+    for num in my_list:
+        if num % 2 == 0:
+            new_list.append(True)
+        else:
+            new_list.append(False)
+
+    return new_list
