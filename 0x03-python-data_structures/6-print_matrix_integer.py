6dbdd15087c69d32e61b43fa18b2ccb6ffe27353
dev-soufiane <dev.sofyane@gmail.com>
2023-11-06 13:54:48 -0800

    Update for loop

--- 0x03-python-data_structures/6-print_matrix_integer.py
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -4,4 +4,4 @@ def print_matrix_integer(matrix=[[]]):
     """Function that prints a matrix of integers."""
 
     for row in matrix:
-        print("{:d} {:d} {:d}".format(*row))
+        print(*("{:d}".format(column) for column in row))
