289908df55053cc2902617fe10d26f0bc7dfaa59
dev-soufiane <dev.sofyane@gmail.com>
2023-11-06 12:35:01 -0800

    Check if my_list is not empty

--- 0x03-python-data_structures/3-print_reversed_list_integer.py
+++ 0x03-python-data_structures/3-print_reversed_list_integer.py
@@ -2,6 +2,7 @@
 
 def print_reversed_list_integer(my_list=[]):
     """Function that prints all integers of a list, in reverse order."""
-    my_list.reverse()
-    for n in my_list:
-        print("{:d}".format(n))
+    if my_list:
+        my_list.reverse()
+        for n in my_list:
+            print("{:d}".format(n))
