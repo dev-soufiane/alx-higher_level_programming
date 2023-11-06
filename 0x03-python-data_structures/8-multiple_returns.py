c848cef5e7fc419e4e7dcfee13842330ce43f12c
dev-soufiane <dev.sofyane@gmail.com>
2023-11-06 14:29:37 -0800

    Check if string is empty

--- 0x03-python-data_structures/8-multiple_returns.py
+++ 0x03-python-data_structures/8-multiple_returns.py
@@ -4,5 +4,7 @@ def multiple_returns(sentence):
     """Function that returns a tuple with the length
     of a string and its first character."""
 
-    if sentence:
-        return len(sentence), sentence[0]
+    if sentence == "":
+        return 0, None
+
+    return len(sentence), sentence[0]
