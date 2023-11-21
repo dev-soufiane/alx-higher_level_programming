f399d2c3e8025a392cd2e2bdbe1a755ffa57189c
dev-soufiane <dev.sofyane@gmail.com>
2023-11-21 10:16:01 -0800

    100-safe_print_integer_err script

---
+++ 0x05-python-exceptions/100-safe_print_integer_err.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+def safe_print_integer_err(value):
+    """
+    Function that prints an integer.
+    """
+    import sys
+    try:
+        print("{:d}".format(value))
+    except (TypeError, ValueError) as error:
+        print("Exception: {}".format(error), file=sys.stderr)
+        return False
+    else:
+        return True
