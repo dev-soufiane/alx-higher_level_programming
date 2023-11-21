86e4d0ce745208cd01534b045e9b524eefdc1b79
dev-soufiane <dev.sofyane@gmail.com>
2023-11-21 10:24:12 -0800

    Change return value

--- 0x05-python-exceptions/101-safe_function.py
+++ 0x05-python-exceptions/101-safe_function.py
@@ -6,7 +6,7 @@ def safe_function(fct, *args):
     import sys
     try:
         res = fct(*args)
+        return res
     except Exception as error:
         print("Exception: {}".format(error), file=sys.stderr)
-        res = None
-    return res
+        return None
