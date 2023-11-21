23603e4900c6dc7817347b3500b94290df925f48
dev-soufiane <dev.sofyane@gmail.com>
2023-11-21 09:27:42 -0800

    change except IndexError to ValueError

--- 0x05-python-exceptions/2-safe_print_list_integers.py
+++ 0x05-python-exceptions/2-safe_print_list_integers.py
@@ -7,8 +7,7 @@ def safe_print_list_integers(my_list=[], x=0):
     for i in range(x):
         try:
             print("{:d}".format(my_list[i]), end='')
+        except (TypeError, ValueError):
             count += 1
-        except (TypeError, IndexError):
-            pass
     print()
     return count
