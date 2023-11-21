22494f150fc0ba250fc35035df10ec85f92176a5
dev-soufiane <dev.sofyane@gmail.com>
2023-11-21 10:03:00 -0800

    Add finally case

--- 0x05-python-exceptions/4-list_division.py
+++ 0x05-python-exceptions/4-list_division.py
@@ -5,11 +5,14 @@ def list_division(my_list_1, my_list_2, list_length):
         try:
             res_list.append(my_list_1[i] / my_list_2[i])
         except TypeError:
-            res_list.append(0.0)
             print("wrong type")
+            res_list.append(0)
         except ZeroDivisionError:
-            res_list.append(0.0)
             print("division by 0")
+            res_list.append(0)
         except IndexError:
             print("out of range")
+            res_list.append(0)
+        finally:
+            None
     return res_list
