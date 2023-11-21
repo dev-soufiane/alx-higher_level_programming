dc87825c1c29db41ea292e0b1277f3bdcc79209b
dev-soufiane <dev.sofyane@gmail.com>
2023-11-21 09:29:37 -0800

    Fix typo

--- 0x05-python-exceptions/2-safe_print_list_integers.py
+++ 0x05-python-exceptions/2-safe_print_list_integers.py
@@ -8,6 +8,7 @@ def safe_print_list_integers(my_list=[], x=0):
         try:
             print("{:d}".format(my_list[i]), end='')
         except (TypeError, ValueError):
-            count += 1
+            continue
+        count += 1
     print()
     return count
