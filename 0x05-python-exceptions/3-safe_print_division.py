e4f02a053b413fb8c100535d3ea996766f32e20d
dev-soufiane <dev.sofyane@gmail.com>
2023-11-21 09:43:05 -0800

    Fix last return indentation

--- 0x05-python-exceptions/3-safe_print_division.py
+++ 0x05-python-exceptions/3-safe_print_division.py
@@ -1,11 +1,10 @@
 #!/usr/bin/python3
 def safe_print_division(a, b):
     """Function that divides 2 integers and prints the result."""
-    res = None
     try:
         res = a / b
     except (ZeroDivisionError, TypeError):
-        pass
+        res = None
     finally:
-        print("Inside result: {:.1f}".format(res))
-    return res
+        print("Inside result: {}".format(res))
+        return res
