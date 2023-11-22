3cee5704d1567631fede4baf2b0ca8e6354b1222
dev-soufiane <dev.sofyane@gmail.com>
2023-11-22 09:18:06 -0800

    Add 1-square script

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+""" Square """
+
+
+class Square:
+    """ A class that represents a Square. """
+
+    def __init__(self, sqr_size):
+        """ Initializing a Square object size. """
+        self.__size = sqr_size
+
