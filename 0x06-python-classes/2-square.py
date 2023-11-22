40100c023697a894a4ad03b7ba5eedcfb6d07e82
dev-soufiane <dev.sofyane@gmail.com>
2023-11-22 09:41:53 -0800

    Add parentheses

--- 0x06-python-classes/2-square.py
+++ 0x06-python-classes/2-square.py
@@ -7,8 +7,8 @@ class Square:
 
     def __init__(self, size=0):
         """ Initiliazing square object. """
-        if not isinstance(size, int):
-            raise TypeError"size must be an integer"
+        if type(size) is not int:
+            raise TypeError("size must be an integer")
         elif size < 0:
-            raise ValueError"size must be >= 0"
+            raise ValueError("size must be >= 0")
         self.__size = size
