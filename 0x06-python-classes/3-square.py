6627650075379cffd1e6c666ea0be7dacaecec09
dev-soufiane <dev.sofyane@gmail.com>
2023-11-22 09:54:44 -0800

    Add 3-square script

---
+++ 0x06-python-classes/3-square.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+""" Area of a square task """
+
+
+class Square:
+    """ A class that defines a square """
+
+    def __int__(self, size=0):
+        """ Initialzing a square object. """
+        if type(size) is not int:
+            raise TypeError("size must be an integer")
+        elif size < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = size
+
+    def area(self):
+        """ Public method that returns the current square area. """
+        return self.__size ** 2
