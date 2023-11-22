c8a459727d04283ab07a598c41ba9989b3b2a9df
dev-soufiane <dev.sofyane@gmail.com>
2023-11-22 09:38:00 -0800

    Add 2-square script

---
+++ 0x06-python-classes/2-square.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+""" Size validation task """
+
+
+class Square:
+    """ A class that represents a square. """
+
+    def __init__(self, size=0):
+        """ Initiliazing square object. """
+        if not isinstance(size, int):
+            raise TypeError"size must be an integer"
+        elif size < 0:
+            raise ValueError"size must be >= 0"
+        self.__size = size
