1048a6691b98c4340ca257c6c753d767c556907f
dev-soufiane <dev.sofyane@gmail.com>
2023-11-22 11:44:57 -0800

    Add 4-square script

---
+++ 0x06-python-classes/4-square.py
@@ -0,0 +1,28 @@
+#!/usr/bin/python3
+""" Access and update private attribute Task """
+
+
+class Square:
+    """ Defines a square """
+
+    def __init__(self, size=0):
+        """ Initialize square object. """
+        self.__size = size
+
+    @property
+    def size(self):
+        """ retrieve the size attribute """
+        return self.__size
+
+    @size.setter
+    def size(self, value):
+        """ set the size attribute """
+        if type(value) is not int:
+            raise TypeError("size must be an integer")
+        elif value < 0:
+            raise ValueError("size must be >= 0")
+        return self.__size = value
+
+    def area(self):
+        """ Returns the current square area """
+        return self.__size ** 2
