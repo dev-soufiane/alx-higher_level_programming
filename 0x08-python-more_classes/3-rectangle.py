f38befc463f105b05889fbff62cf1f2ebb9346ce
dev-soufiane <dev.sofyane@gmail.com>
2023-11-27 14:49:28 -0800

    Fix indented block

--- 0x08-python-more_classes/3-rectangle.py
+++ 0x08-python-more_classes/3-rectangle.py
@@ -55,8 +55,8 @@ class Rectangle:
             return 0
         return (self.__width + self.__height) * 2
 
-        def __str__(self):
-        """ Prints out the rectangle """
-        if self.__width == 0 or self.__height == 0:
-            return ""
-        return (('#'*self.__width + "\n")*self.__height)[:-1]
+    def __str__(self):
+    """ Prints out the rectangle """
+    if self.__width == 0 or self.__height == 0:
+        return ""
+    return (('#'*self.__width + "\n")*self.__height)[:-1]
