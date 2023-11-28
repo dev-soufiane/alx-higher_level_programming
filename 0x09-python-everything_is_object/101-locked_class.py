0b4e6723f85e8facc3749ab8c8ae3ed1060b8c52
dev-soufiane <dev.sofyane@gmail.com>
2023-11-28 13:28:44 -0800

    Add 101-locked_class script

---
+++ 0x09-python-everything_is_object/101-locked_class.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+""" Low memory cost """
+
+
+class LockedClass:
+    """
+    Prevents the user from dynamically creating new instance attributes,
+    except if the new instance attribute is called first_name
+    """
+
+    __slots__ = ["first_name"]
