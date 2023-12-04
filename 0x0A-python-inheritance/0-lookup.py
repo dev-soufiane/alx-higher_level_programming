e7e03ca13b246a7f94fc35ac53fe07e14a9aa258
dev-soufiane <dev.sofyane@gmail.com>
2023-12-04 08:58:30 -0800

    Add 0-lookup script

---
+++ 0x0A-python-inheritance/0-lookup.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+"""
+    This module returns the list of available
+    attributes and methods of an object
+"""
+
+
+def lookup(obj):
+    """ Returns a list object """
+    return dir(obj)
