abb5d17b3345878989c2f9a8c82c943ec5c0a232
dev-soufiane <dev.sofyane@gmail.com>
2023-12-04 09:55:50 -0800

    Add 6-base_geometry script

---
+++ 0x0A-python-inheritance/6-base_geometry.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+"""
+    This module defines a BaseGeometry class.
+"""
+
+
+class BaseGeometry:
+    """
+    Defines BaseGeometry class.
+    """
+
+    def area(self):
+        """
+        Raises an Exception with the message area() is not implemented.
+        """
+        raise Exception("area() is not implemented")
