0627cbcae3ced10be05de0f1f3f5c809fbc8f437
dev-soufiane <dev.sofyane@gmail.com>
2023-12-04 09:23:44 -0800

    ADD 2-is_same_class script

---
+++ 0x0A-python-inheritance/2-is_same_class.py
@@ -0,0 +1,12 @@
+#!/user/bin/python3
+"""
+    This module checks if an object is an instance of a class.
+"""
+
+
+def is_same_class(obj, a_class):
+    """
+    Returns True if the object is exactly an instance
+    of the specified class ; otherwise False.
+    """
+    return isinstance(obj, a_class):
