4610b2489d65af9b82b538f67dfd0d46472d11ad
dev-soufiane <dev.sofyane@gmail.com>
2023-12-04 10:55:17 -0800

    remove duplicate

--- 0x0A-python-inheritance/10-square.py
+++ 0x0A-python-inheritance/10-square.py
@@ -14,7 +14,6 @@ class Square(Rectangle):
         """
         Initializes a square object.
         """
-    super().__init__(size, size)
     self.integer_validator("size", size)
     super().__init__(size, size)
     self.__size = size
