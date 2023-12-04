ef93e5d0e527f67a3e16e5f6059261f46a7179bf
dev-soufiane <dev.sofyane@gmail.com>
2023-12-04 10:06:23 -0800

    Add 7-base_geometry script

---
+++ 0x0A-python-inheritance/7-base_geometry.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+"""
+    This module defines a class BaseGeometry.
+"""
+
+
+class BaseGeometry:
+    """
+    Defines a BaseGeometry class.
+    """
+
+    def area(self):
+        """
+        Raises an Exception with the message area() is not implemented.
+        """
+        raise Exception("area() is not implemented")
+
+    def integer_validator(self, name, value):
+        """ Validates integer value. """
+        if type(value) is not int:
+            raise TypeError(f"{name} must be an integer")
+        if value <= 0:
+            raise ValueError(f"{name} must be greater than 0")
