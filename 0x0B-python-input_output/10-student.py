7b2cd6b2e8e91ad097a1b2df017c9862f3347c65
dev-soufiane <dev.sofyane@gmail.com>
2023-12-06 14:58:50 -0800

    Update 10-student.py

--- 0x0B-python-input_output/10-student.py
+++ 0x0B-python-input_output/10-student.py
@@ -13,17 +13,17 @@ class Student:
         self.first_name = first_name
         self.last_name = last_name
         self.age = age
-    
+
     def to_json(self, attrs=None):
         """ Retrieves a dictionary representation of a Student. """
         res = {}
 
-    if attrs is None:
-        return self.__dict__
+        if attrs is None:
+            return self.__dict__
 
-    for attr in attrs:
-        value = self.__dict__.get(attr)
-        if value is not None:
-            res[attr] = value
+        for attr in attrs:
+            value = self.__dict__.get(attr)
+            if value is not None:
+                res[attr] = value
 
-    return res
+        return res
