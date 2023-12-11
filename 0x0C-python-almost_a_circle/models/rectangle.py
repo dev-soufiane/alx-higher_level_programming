3966f9285f0fe0a2639928a9b05255b247cc1b8c
dev-soufiane <dev.sofyane@gmail.com>
2023-12-11 08:46:46 -0800

    Improve Rectangle class attribute validation

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -22,6 +22,10 @@ class Rectangle(Base):
     @width.setter
     def width(self, value):
         """ sets width attribute. """
+        if type(value) is not int:
+            raise TypeError("width must be an integer")
+        elif value <= 0:
+            raise ValueError("width must be > 0")
         self.__width = value
 
     @property
@@ -32,6 +36,10 @@ class Rectangle(Base):
     @height.setter
     def height(self, value):
         """ sets height attribute. """
+        if type(value) is not int:
+            raise TypeError("height must be an integer")
+        elif value <= 0:
+            raise ValueError("height must be > 0")
         self.__height = value
 
     @property
@@ -42,6 +50,8 @@ class Rectangle(Base):
     @x.setter
     def x(self, value):
         """ sets x attribute. """
+        if value < 0:
+            raise ValueError("x must be >= 0")
         self.__x = value
 
     @property
@@ -52,4 +62,6 @@ class Rectangle(Base):
     @y.setter
     def y(self, value):
         """ sets y attribute. """
+        if value < 0:
+            raise ValueError("y must be >= 0")
         self.__y = value
