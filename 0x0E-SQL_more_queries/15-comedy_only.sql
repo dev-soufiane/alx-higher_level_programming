05a10b5a5de4814d95aa0693df5af9e3dec8be30
dev-soufiane <dev.sofyane@gmail.com>
2024-01-17 14:24:37 -0800

    Add 15-comedy_only.sql

---
+++ 0x0E-SQL_more_queries/15-comedy_only.sql
@@ -0,0 +1,11 @@
+-- Lists all comedy shows in the database hbtn_0d_tvshows
+-- 'tv_genres' table contains only one record where name = Comedy
+
+SELECT tv_shows.title
+FROM tv_shows
+INNER JOIN tv_show_genres
+ON tv_shows.id = tv_show_genres.show_id
+INNER JOIN tv_genres
+ON tv_show_genres.genre_id = tv_genres.id
+WHERE tv_genres.name = "Comedy"
+ORDER BY tv_shows.title;
