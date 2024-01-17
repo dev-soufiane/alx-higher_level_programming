aeebb9d2dbd8ca3ffb7049b0c840b032adfda3ea
dev-soufiane <dev.sofyane@gmail.com>
2024-01-17 14:26:38 -0800

    Add 16-shows_by_genre.sql

---
+++ 0x0E-SQL_more_queries/16-shows_by_genre.sql
@@ -0,0 +1,11 @@
+-- List all shows and genres linked to show from 'hbtn_0d_tvshows'
+-- If show doesn't have a genre, display NULL in genre column
+-- Each record should display tv_shows.title, tv_genres.name
+
+SELECT tv_shows.title, tv_genres.name
+FROM tv_shows
+LEFT JOIN tv_show_genres
+ON tv_shows.id = tv_show_genres.show_id
+LEFT JOIN tv_genres
+ON tv_show_genres.genre_id = tv_genres.id
+ORDER BY tv_shows.title, tv_genres.name;
