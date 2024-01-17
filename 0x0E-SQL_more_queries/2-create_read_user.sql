898cd52f4166ee4d3b616d7c6641d2f2dc06b139
dev-soufiane <dev.sofyane@gmail.com>
2024-01-17 13:27:57 -0800

    Add 2-create_read_user.sql

---
+++ 0x0E-SQL_more_queries/2-create_read_user.sql
@@ -0,0 +1,6 @@
+-- script that creates the database hbtn_0d_2 and the user user_0d_2
+-- If the database hbtn_0d_2 already exists, the script should not fail
+-- If the user user_0d_2 already exists, the script should not fail
+CREATE DATABASE IF NOT EXISTS hbtn_0d_2;
+CREATE USER IF NOT EXISTS 'user_0d_2'@'localhost' IDENTIFIED BY 'user_0d_2_pwd';
+GRANT SELECT ON hbtn_0d_2.* TO 'user_0d_2'@'localhost';
