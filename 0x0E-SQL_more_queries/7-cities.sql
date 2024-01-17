5fe7dde00d340573ddd9fcc37d865b68c3bdbafe
dev-soufiane <dev.sofyane@gmail.com>
2024-01-17 14:15:08 -0800

    Update 7-cities.sql

--- 0x0E-SQL_more_queries/7-cities.sql
+++ 0x0E-SQL_more_queries/7-cities.sql
@@ -2,10 +2,7 @@
 -- If the database hbtn_0d_usa already exists, your script should not fail
 -- If the table cities already exists, your script should not fail
 CREATE DATABASE IF NOT EXISTS hbtn_0d_usa;
-USE hbtn_0d_usa;
-CREATE TABLE IF NOT EXISTS cities (
-    id INT UNIQUE AUTO_INCREMENT,
-    state_id INT NOT NULL,
-    name VARCHAR(256) NOT NULL,
-    FOREIGN KEY (state_id) REFERENCES states(id),
-    PRIMARY KEY (id)
+CREATE TABLE IF NOT EXISTS hbtn_0d_usa.cities (id INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
+state_id INT NOT NULL,
+name VARCHAR(256) NOT NULL,
+FOREIGN KEY(state_id) REFERENCES hbtn_0d_usa.states(id));
