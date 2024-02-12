7f3a33bbb255654297a69ac41079b420998a7429
dev-soufiane <dev.sofyane@gmail.com>
2024-02-12 14:10:24 -0800

    Make script executable

--- 0x12-javascript-warm_up/5-to_integer.js
+++ 0x12-javascript-warm_up/5-to_integer.js
@@ -1,2 +1,6 @@
 #!/usr/bin/node
-console.log(parseInt(process.argv[2]) ? `My number: ${parseInt(process.argv[2])}` : 'Not a number');
+if (Number(process.argv[2])) {
+  console.log(`My number: ${Number(process.argv[2])}`);
+} else {
+  console.log('Not a number');
+}
