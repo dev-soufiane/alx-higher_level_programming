c6c27c7f70e4630b36fdfc1a6e936453cda73da0
dev-soufiane <dev.sofyane@gmail.com>
2024-02-22 14:41:12 -0800

    Update 102-relationship_cities_states_list

--- 0x0F-python-object_relational_mapping/102-relationship_cities_states_list.py
+++ 0x0F-python-object_relational_mapping/102-relationship_cities_states_list.py
@@ -1,32 +1,24 @@
 #!/usr/bin/python3
-"""State city module."""
-from sqlalchemy.orm import sessionmaker
-from sqlalchemy import create_engine
+'''From city.'''
+import sys
+from sqlalchemy import create_engine, and_, text, tuple_
+from sqlalchemy.orm import sessionmaker, relationship
+
 from relationship_state import Base, State
 from relationship_city import City
-import sys
 
 
-def fetch_city_state():
-    """Gets all states cities in database."""
-    username = sys.argv[1]
-    password = sys.argv[2]
-    database = sys.argv[3]
-
-    engine = create_engine('mysql+mysqldb://{}:{}@localhost:3306/{}'.format(
-        username, password, database), pool_pre_ping=True)
-
-    Base.metadata.create_all(engine)
-    Session = sessionmaker()
-    Session.configure(bind=engine)
-    session = Session()
-
-    for city in session.query(City).order_by(City.id).all():
-        print("{}: {} -> {}".format(city.id, city.name, city.state.name))
-
-    session.commit()
-    session.close()
-
-
-if __name__ == "__main__":
-    fetch_city_state()
+if __name__ == '__main__':
+    if len(sys.argv) >= 4:
+        user = sys.argv[1]
+        pword = sys.argv[2]
+        db_name = sys.argv[3]
+        DATABASE_URL = 'mysql://{}:{}@localhost:3306/{}'.format(
+            user, pword, db_name
+        )
+        engine = create_engine(DATABASE_URL)
+        Base.metadata.create_all(engine)
+        session = sessionmaker(bind=engine)()
+        q = session.query(City).join(State).order_by(City.id.asc())
+        for city in q.all():
+            print('{}: {} -> {}'.format(city.id, city.name, city.state.name))
