e24d5675d8681a7ffb8cf426164a8e3e9d734f3e
dev-soufiane <dev.sofyane@gmail.com>
2024-02-23 15:31:45 -0800

    Update 102-relationship_cities_states_list.py

--- 0x0F-python-object_relational_mapping/102-relationship_cities_states_list.py
+++ 0x0F-python-object_relational_mapping/102-relationship_cities_states_list.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python3
-'''From city.'''
+'''Prints all City objects and their State in a database.
+'''
 import sys
 from sqlalchemy import create_engine, and_, text, tuple_
 from sqlalchemy.orm import sessionmaker, relationship
