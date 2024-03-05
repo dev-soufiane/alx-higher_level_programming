a2d78caa3db367d8e2ce60a8e1dcc6f1c6a097eb
dev-soufiane <dev.sofyane@gmail.com>
2024-03-05 14:00:31 -0800

    Add 8-json_api.py

---
+++ 0x11-python-network_1/8-json_api.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python3
+"""Sends a POST request to http://0.0.0.0:5000/search_user with a given letter.
+Usage: ./8-json_api.py <letter>
+  - The letter is sent as the value of the variable `q`.
+  - If no letter is provided, sends `q=""`.
+"""
+import sys
+import requests
+
+if __name__ == "__main__":
+    if len(sys.argv) > 1:
+        arg = sys.argv[1]
+    else:
+        arg = ""
+    payload = {'q': arg}
+    url = "http://0.0.0.0:5000/search_user"
+    r = requests.post(url, data=payload)
+    try:
+        r.raise_for_status()
+        json = r.json()
+        if len(json) == 0:
+            print("No result")
+        else:
+            print("[{:d}] {}".format(json['id'], json['name']))
+    except Exception:
+        print("Not a valid JSON")
