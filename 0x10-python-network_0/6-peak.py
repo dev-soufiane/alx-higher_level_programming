2f4865d8a94396e76c31237990a67fb677e4911d
dev-soufiane <dev.sofyane@gmail.com>
2024-03-05 13:38:26 -0800

    Update 6-peak.py

--- 0x10-python-network_0/6-peak.py
+++ 0x10-python-network_0/6-peak.py
@@ -1,10 +1,32 @@
 #!/usr/bin/python3
-""" Test function find_peak """
-find_peak = __import__('6-peak').find_peak
+""" Finds a peak in a list of unsorted integers using a binary search approach
+"""
 
-print(find_peak([1, 2, 4, 6, 3]))
-print(find_peak([4, 2, 1, 2, 3, 1]))
-print(find_peak([2, 2, 2]))
-print(find_peak([]))
-print(find_peak([-2, -4, 2, 1]))
-print(find_peak([4, 2, 1, 2, 2, 2, 3, 1]))
+
+def find_peak(list_of_integers):
+    """
+    Args:
+        list_of_integers (list): List of integers to find the peak of.
+    Returns:
+        int: Peak of list_of_integers or None if the list is empty.
+    """
+    size = len(list_of_integers)
+    mid_end = size
+    mid = size // 2
+
+    if size == 0:
+        return None
+
+    while True:
+        mid_end = mid_end // 2
+        if (mid < size - 1 and
+                list_of_integers[mid] < list_of_integers[mid + 1]):
+            if mid_end // 2 == 0:
+                mid_end = 2
+            mid = mid + mid_end // 2
+        elif mid_end > 0 and list_of_integers[mid] < list_of_integers[mid - 1]:
+            if mid_end // 2 == 0:
+                mid_end = 2
+            mid = mid - mid_end // 2
+        else:
+            return list_of_integers[mid]
