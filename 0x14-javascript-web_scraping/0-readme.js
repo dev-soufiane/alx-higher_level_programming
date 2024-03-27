58dbcd92a860952a1013af13c73c2a935f41a523
dev-soufiane <dev.sofyane@gmail.com>
2024-03-26 18:49:40 -0700

    Add 0-readme.js

---
+++ 0x14-javascript-web_scraping/0-readme.js
@@ -0,0 +1,7 @@
+#!/usr/bin/node
+// script reads and prints the content of a file.
+
+const fs = require('fs');
+fs.readFile(process.argv[2], 'utf8', function (error, content) {
+  console.log(error || content);
+});
