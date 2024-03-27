0dae56541c545752b189caeea5250ce10ad3c768
dev-soufiane <dev.sofyane@gmail.com>
2024-03-26 19:00:52 -0700

    Add 101-starwars_characters.js

---
+++ 0x14-javascript-web_scraping/101-starwars_characters.js
@@ -0,0 +1,23 @@
+#!/usr/bin/node
+// script prints all characters of a Star Wars movie displaying one character
+// name by line in the same order of the list “characters” in the /films/ response
+
+const request = require('request');
+const url = 'https://swapi-api.hbtn.io/api/films/' + process.argv[2];
+request(url, function (error, response, body) {
+  if (!error) {
+    const characters = JSON.parse(body).characters;
+    printCharacters(characters, 0);
+  }
+});
+
+function printCharacters (characters, index) {
+  request(characters[index], function (error, response, body) {
+    if (!error) {
+      console.log(JSON.parse(body).name);
+      if (index + 1 < characters.length) {
+        printCharacters(characters, index + 1);
+      }
+    }
+  });
+}
